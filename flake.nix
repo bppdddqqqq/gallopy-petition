1feac39d2663ab33d3e5a30264d4e193ad0eaa54
Adam Parak <definitelynotmymail@gmail.com>
2023-04-18 14:31:38 +0200

    add nix stuff

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, ...}@attrs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs-18_x
+        nodePackages.npm
+        python38
+        autoreconfHook
+        libpng
+        yarn
+      ];
+    };
+  };
+}
