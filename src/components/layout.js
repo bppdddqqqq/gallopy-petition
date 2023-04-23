1c630a9ec9b848df35911c5951c91cec0e1f1859
Adam Parak <definitelynotmymail@gmail.com>
2023-04-24 00:08:19 +0200

    chore - remove old code

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,13 @@
 import React from 'react'
-// import { useStaticQuery, graphql } from "gatsby"
 
 import Navigation from "./navigation";
 
 import Footer from "./footer";
 
 
-// const query = graphql`
-// query LayoutQuery {
-//   site {
-//     siteMetadata {
-//       siteTitle: title
-//     }
-//   }
-// }
-// `
+
 
 const Layout = ({children, className = "container mx-auto my-0 text-left min-h-[80vh]"}) => {
-
-  // const { site } = useStaticQuery(query)
-  // const { siteTitle } = site.siteMetadata
-
   return (
     <main>
       <Navigation />
