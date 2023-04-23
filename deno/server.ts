1fb8ebd74581f3bbc424d8d0eb00af54cc4600e7
Adam Parak <xparak1@fi.muni.cz>
2023-04-23 22:01:19 +0200

    feat - add strings to server

--- deno/server.ts
+++ deno/server.ts
@@ -1,5 +1,6 @@
 import { DataTypes, Database, Model, SQLite3Connector } from 'https://deno.land/x/denodb_updated/mod.ts';
 
+if 
 const conn = new SQLite3Connector({
     filepath: 'app.db'
 })
@@ -21,6 +22,8 @@ class Signatures extends Model {
       },
       consent: DataTypes.BOOLEAN,
       job: DataTypes.STRING,
+      city: DataTypes.STRING,
+      ip: DataTypes.STRING,
     
       token: DataTypes.STRING,
     };
@@ -29,16 +32,41 @@ class Signatures extends Model {
 db.link([Signatures]);
 
 //todo: remove drop when in prod!
-// await db.sync({ drop: true });
+await db.sync({ drop: env['DENO_PRODUCTION_MODE']=='FALSE' });
 // await db.sync()
 
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { valid as emailValid } from "https://deno.land/x/validation@v0.4.0/email.ts";
 import * as uuid from "https://deno.land/std/uuid/mod.ts";
 import { Zoic } from "https://deno.land/x/zoic/zoic.ts";
+import LRU from "https://deno.land/x/lru_cache@6.0.0-deno.4/mod.ts";
+import { SmtpClient } from "https://deno.land/x/smtp/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const app = new Application();
 const router = new Router();
+
+const ipCache = new LRU<any, any>({
+    max: 4096
+    , length: (n, key) => 1
+    , dispose: (key, n) =>  (0)
+    , maxAge: 1000 * 15
+});
+
+import { readCSVObjects } from "https://deno.land/x/csv/mod.ts";
+import env from 'https://deno.land/x/sqlite@v3.1.3/build/vfs.js';
+
+let awsUsername = '';
+let awsPassword = ''
+if (env['DENO_PRODUCTION_MODE'] != 'TRUE') {
+    const f = await Deno.open("./credentials.csv");
+    for await (const obj of readCSVObjects(f)) {
+        awsUsername = obj['Smtp Username']
+        awsPassword = obj['Smtp Password\r']
+    }
+}
+
+const client = new SmtpClient();
 
 router.get('/', (ctx) => {
     ctx.response.body = 'hello!';
@@ -52,33 +80,50 @@ router.post('/sign', async (ctx) => {
         console.log(formData)
         // the form data is fully available
         const { fields } = formData;
-        const { email, firstname, lastname, job } = fields
-        if (!emailValid(email)) {
-            ctx.response.body = 'bad email!';
+        const { email, firstname, lastname, job, city } = fields
+
+        if (ipCache.has(ctx.request.ip)) {
+            ctx.response.body = 'IP adresa jiz byla pouzita v jine zadosti!';
+            return;
+        }
+        if (!emailValid(email) || await Signatures.where("email", email).select("email").count() > 0) {
+            ctx.response.body = 'Zly format emailu nebo email byl pouzit!';
             return;
         }
 
         const consent = (fields.consent === "1");
         const token = uuid.v1.generate().toString();
 
-        try {
-            await Signatures.create({
-                firstName: firstname,
-                lastName: lastname,
-                email,
-                job,
-                consent,
-                token,
-            })
-        } catch (e) {
-            ctx.response.body = 'nok!';
-            return;
-        }
-        console.log(token, await Signatures.count())
+        await client.connectTLS({
+            hostname: env['DENO_SERV'] || "email-smtp.us-east-1.amazonaws.com",
+            port: env['DENO_PORT'] || 465,
+            username: env['DENO_USER'] || awsUsername,
+            password: env['DENO_PASS'] || awsPassword,
+          }).then(() => console.log('SMTP connected!'));
+        await client.send({
+            from: "scala.noreply@gmail.com",
+            to: email,
+            subject: "[Scala Petice] Zadost o potvrzeni emailu pro podpis",
+            content: ".",
+            html: `Dobry den, posilame vam informaci, ze jste na nasem webu podepsal petici a je nutne pro dokonceni podpisu potvrdit email touhle spravou. Pro potvrzeni stlacte odkaz nize <a href='http://localhost:8080/confirm?email=${email}&token=${token}'>http://localhost:8080/confirm?email=${email}&token=${token}</a>`,
+        });
+        await client.close()
+        await Signatures.create({
+            firstName: firstname,
+            lastName: lastname,
+            email,
+            job,
+            consent,
+            token,
+            ip: ctx.request.ip,
+            city,
+        })
+        ipCache.set(ctx.request.ip, '')
+        console.log(email, token)
         
-        ctx.response.body = 'submitted!';
+        ctx.response.body = 'Odevzdáno!!';
     } else {
-        ctx.response.body = 'nok!';
+        ctx.response.body = 'Zlá žádost!';
     }
 });
 
@@ -86,7 +131,7 @@ router.get('/confirm', async (ctx) => {
     const token = ctx.request.url.searchParams.get('token') || ''
     const email = ctx.request.url.searchParams.get('email') || ''
     if (email == '' || token == '' || !emailValid(email)) {
-        ctx.response.body = 'bad request';
+        ctx.response.body = 'Zlá žádost!';
         return;
     }
 
@@ -101,24 +146,19 @@ router.get('/confirm', async (ctx) => {
 const countCache = new Zoic({
     cache: 'LFU',
     expire: '5m, 3s',
-    capacity: 1,
-});
-
-const nameCache = new Zoic({
-    cache: 'LFU',
-    expire: '15m, 3s',
-    capacity: 1,
+    capacity: 15,
 });
 
 router.get('/count', countCache.use, async (ctx) => {
     ctx.response.body = await Signatures.where('token', '').count()
 })
 
-router.get('/names', nameCache.use, async (ctx) => {
+router.get('/names', countCache.use, async (ctx) => {
     const names = await Signatures.where("consent", true).where("token", "").select("firstName", "lastName", "job").limit(10).orderBy("id", "desc").get()
     ctx.response.body = JSON.stringify(names)
 })
 
+app.use(oakCors()); // Enable CORS for All Routes
 app.use(router.routes());
 app.use(router.allowedMethods());
 await app.listen({ port: 8080 });
