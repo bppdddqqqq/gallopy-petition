c3cb9bb7972ab1d1bae48a37fc2bb53ced19637b
Adam Parak <xparak1@fi.muni.cz>
2023-04-27 17:58:42 +0200

    fix footer responsitivity

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,13 +5,13 @@ import ScalaLogo from '../../static/assets/scala-vert.svg'
 
 const Footer = () => (
   <footer
-    className="py-3 bg-red-400"
+    className="py-5 bg-red-400"
   >
-    <div className="container mx-auto max-w-6xl md:max-w-4xl w-full grid grid-cols-3 max-md:grid-cols-2 gap-4 text-white ">
+    <div className="container mx-auto max-w-6xl md:max-w-4xl w-full md:grid md:grid-cols-3 flex flex-wrap gap-4 text-white ">
       <div className="max-md:self-center max-md:justify-self-center">
         <ScalaLogo className="overflow-hidden h-24 w-24"/>
       </div>
-      <div className="">
+      <div className="flex-grow">
         <h3 className="font-bold">Kontakty:</h3>
         <ul className="list-none text-md leading-6">
           <li className="list-none ml-1"><a href="mailto:scalavescale@gmail.com">scalavescale@gmail.com</a></li>
@@ -19,7 +19,7 @@ const Footer = () => (
           <li className="list-none ml-1"><a href="mailto:daliborjelinek01@gmail.com">Dalibor Jelínek</a>, +420&nbsp;728&nbsp;947&nbsp;279</li>
         </ul>
       </div>
-      <div className="text-white fill-white text-6xl flex justify-center md:justify-end max-md:col-span-2 md:self-center">
+      <div className="text-white fill-white text-6xl flex justify-center flex-1 md:justify-end md:self-center">
           {/* <a href="" target="_blank"><RiFacebookBoxFill /></a> */}
           <a href="https://www.instagram.com/scala_ve_scale" target="_blank" rel="noreferrer"><RiInstagramFill /></a>
       </div>
